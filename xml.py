e83640c18e3e4d1e86893a66d9f015a0b91181e4
David Santos <35598098+davidsantos777@users.noreply.github.com>
2019-03-10 22:54:47 +0100

    Add files via upload

---
+++ xml.py
@@ -0,0 +1,11 @@
+from lxml import etree
+doc = etree.parse('ciclismo.xml')
+while True:
+	print('''1.- Lista todos los nombres de las rutas cuya ID de dificultad sea 144.
+2.- Contar todos los recorridos que no formen parte del camino de santiago.
+3.- Mostrar la Id del recurso de categoría de las rutas cuya longitud está entre un límite inferior y otro superior.
+4.- Pedir codigo de recurso y mostrar nombre de la ruta asociado a dicho codigo.
+5.- Pedir por teclado una zona, mostrar las rutas que formen parte de dicha zona junto con su dificultad y mostrar si tienen o no diploma.
+0.- Salir''')
+
+	opcion = input("Opción:")
